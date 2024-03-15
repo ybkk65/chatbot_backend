236f7923dd992a387f0117854e615301a7062e2f
sxno der toten <146814956+sxno-der-toten@users.noreply.github.com>
2024-03-15 16:26:26 +0100

    correction

--- Router.php
+++ Router.php
@@ -12,7 +12,44 @@ class Router {
         $this->run();
     }
 
-    protected function run() {
-        echo 'router';
+    protected function extractParams(string $url, string $rule) {
+        $params = [];
+        $urlParts = explode('/', trim($url, '/'));
+        $ruleParts = explode('/', trim($rule, '/'));
+    
+        foreach ($ruleParts as $index => $rulePart) {
+            if (strpos($rulePart, ':') === 0 && isset($urlParts[$index])) {
+                $paramName = substr($rulePart, 1);
+                $params[$paramName] = $urlParts[$index];
+            }
+        }
+    
+        return $params;
+    }    
+
+    protected function matchRule(string $url, string $rule) {
+        (array) $urlParts = explode ('/', trim ($url, '/'));
+        (array) $ruleParts = explode ('/', trim ($rule, '/'));
+
+        if (count($urlParts) !== count($ruleParts)) {
+            return false;
+        }
+
+        foreach($ruleParts as $index => $rulePart) {
+            if ($rulePart !== $ruleParts($index) && strpos($rulePart, ':') !== 0) {
+                return false;
+            }
+        }
+
+        return true;
+     }
+
+     protected function run() {
+        foreach ($this->routes as $route => $controller) { 
+            if ($this->matchRule($this->url, $route)) {
+                echo ' | ' . $route . ' ' . $controller;
+                $this->extractParams($this->url, $route);
+            }
+        }
     }
 }
